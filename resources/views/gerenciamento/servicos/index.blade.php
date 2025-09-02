c1d1c9d3b3c63008ca460f871621e55539e848b2
luizalvesot <alvesluizotavio23@gmail.com>
2025-09-01 22:11:01 -0300

    ajustado botão no centro da tela

--- resources/views/gerenciamento/servicos/index.blade.php
+++ resources/views/gerenciamento/servicos/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="container-sm">
         <div class="row pt-3 mb-2">
-            <div class="col-md text-end">
+            <div class="col-md text-center">
                 <a href="{{route('servicos.create')}}" title="Cadastrar serviços" class="btn btn-sm btn-primary px-3">
                     Cadastrar Serviço
                     <i class="bi bi-plus-lg"></i>
